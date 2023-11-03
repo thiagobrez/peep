375055165bd6d00daa7f31011fef2f03f179f024
Thiago Brezinski <thiagobrez@gmail.com>
2023-11-03 19:24:05 +0000

    chore: install supabase

---
+++ src/utils/supabase.ts
@@ -0,0 +1,17 @@
+import 'react-native-url-polyfill/auto';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { createClient } from '@supabase/supabase-js';
+import Constants from 'expo-constants';
+
+export const supabase = createClient(
+  Constants.expoConfig.extra.EXPO_PUBLIC_SUPABASE_URL,
+  Constants.expoConfig.extra.EXPO_PUBLIC_SUPABASE_ANON_KEY,
+  {
+    auth: {
+      storage: AsyncStorage,
+      autoRefreshToken: true,
+      persistSession: true,
+      detectSessionInUrl: false,
+    },
+  },
+);
